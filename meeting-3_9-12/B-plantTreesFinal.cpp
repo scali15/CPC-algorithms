96614d90ca0b43af3d833ede5ece38bc3382a90c
scali15 <122848170+scali15@users.noreply.github.com>
2024-09-19 15:41:13 -0400

    Almost finished last weeks (meeting 9/12) problem C

--- meeting-3_9-12/B-plantTreesFinal.cpp
+++ meeting-3_9-12/B-plantTreesFinal.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <sstream>
 #include <vector>
@@ -32,39 +31,4 @@ int main() {
     maxTime += 1;
     cout << maxTime;
     return 0;
-=======
-#include <iostream>
-#include <sstream>
-#include <vector>
-#include <algorithm> // for sort and max
-#include <functional> // for greater<int>()
-
-using namespace std;
-
-int main() {   
-    int n;
-    cin >> n;
-    cin.ignore();
-    
-    string line;
-    getline(cin, line);  // Read a line of input
-
-    istringstream iss(line);
-    vector<int> numbers;
-    int num;
-
-    // Parse the line and add integers to the vector
-    while (iss >> num) {
-        numbers.push_back(num);
-    }
-
-    sort(numbers.begin(), numbers.end(), greater<int>());
-    int maxTime = 0;
-    for (int i = 1; i <= n; i++){
-        maxTime = max(maxTime, numbers[i-1] + i);
-    }
-    maxTime += 1;
-    cout << maxTime;
-    return 0;
->>>>>>> abb5cda728a614a306588de17fcc5b4b675d632f
 }
--- meeting-3_9-12/C-airConditionedMinions.cpp
+++ meeting-3_9-12/C-airConditionedMinions.cpp
@@ -1,10 +1,72 @@
 #include <iostream>
-#include <string>
+#include <queue>
+#include <vector>
+#include <sstream>
 
 using namespace std;
+
+// Define custom comparator for min heap sorted by narrowest to widest range of minions preferred tempuratures
+auto cmp1 = [](const pair<int, int>& a, const pair<int, int>& b) {
+    // Compare pairs by the second element, time willing to wait
+    return (a.second - a.first) > (b.second - b.first);
+};
+
+// Function to count the number of minions a certain integer value will satisy
+int skewerCount(priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp1)> rectangles, int skewer){
+    int count = 0;
+    while(!rectangles.empty()){
+        if (skewer >= rectangles.top().first && skewer <= rectangles.top().second){
+            count++;
+        }
+        rectangles.pop();
+    }
+    return count;
+}
+
+void removeStabbed(priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp1)>& rectangles, int skewer){
+    priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp1)> rCopy(cmp1);
+    rectangles.swap(rCopy);
+    while(!rCopy.empty()){
+        pair<int, int> curr = rCopy.top();
+        rCopy.pop();
+        if (skewer < curr.first || skewer > curr.second){
+            rectangles.push(curr);
+        }
+    }
+}
+
 int main() {
+
+    /* ALMOST DONE, JUST GOTTA DO THE INPUT AND THIS SHOULD WORK 
     
+    FINISH LATER THOUGH BECAUSE I GOPTTA GET TO INFO SECURITY CLASS */
 
+    // Min heap implemented with priority queue below
+    priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp1)> preferredTemps(cmp1);
+    // sorted queue ordered 
+    priority_queue preferredTempsCopy(preferredTemps);
+    vector<pair<int, int>> rectangles;
 
+    pair<int, int> strictestMinion;
+    int numSkewers = 0;
+    while(!preferredTemps.empty()){
+        numSkewers++;
+        strictestMinion = preferredTemps.top();
+        preferredTemps.pop();
+
+        int mostStabbed = -1;
+        int stabCount = 0;
+        int bestSkewer;
+        for (int i = strictestMinion.first; i <= strictestMinion.second; i++){
+            stabCount = skewerCount(preferredTemps, i);
+            if (stabCount > mostStabbed){
+                mostStabbed = stabCount;
+                bestSkewer = i;
+            }
+        }
+        removeStabbed(preferredTemps, bestSkewer);
+    }
+
+    cout << numSkewers;
     return 0;
 }
